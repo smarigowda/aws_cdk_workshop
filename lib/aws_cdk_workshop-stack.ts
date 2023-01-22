59e7b83e25278cfc17c87602ca93910855c92522
Santosh Marigowda <santosh.marigowda@abcam.com>
2023-01-22 18:31:13 +0000

    using HitCounter construct inside our stack.

--- lib/aws_cdk_workshop-stack.ts
+++ lib/aws_cdk_workshop-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from "aws-cdk-lib";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as apigw from "aws-cdk-lib/aws-apigateway";
+import { HitCounter } from './hitcounter';
 
 export class AwsCdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -12,8 +13,17 @@ export class AwsCdkWorkshopStack extends cdk.Stack {
       handler: "hello.handler",
     });
 
+    // new apigw.LambdaRestApi(this, 'Endpoint', {
+    //   handler: hello
+    // })
+
+    const helloWithCounter = new HitCounter(this, 'HelloHitCounter', {
+      downstream: hello
+    });
+
+    // defines an API Gateway REST API resource backed by our "hello" function.
     new apigw.LambdaRestApi(this, 'Endpoint', {
-      handler: hello
-    })
+      handler: helloWithCounter.handler
+    });
   }
 }
