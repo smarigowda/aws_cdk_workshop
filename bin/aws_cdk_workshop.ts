a49b7e7c2dde597650a4f5a3771fa765174fe723
Santosh Marigowda <santosh.marigowda@abcam.com>
2023-01-24 16:15:06 +0000

    updated deployment entrypoint

--- bin/aws_cdk_workshop.ts
+++ bin/aws_cdk_workshop.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { AwsCdkWorkshopStack } from '../lib/aws_cdk_workshop-stack';
+import { WorkshopPipelineStack } from '../lib/pipeline-stack';
 
 const app = new cdk.App();
-new AwsCdkWorkshopStack(app, 'AwsCdkWorkshopStack');
+// new AwsCdkWorkshopStack(app, 'AwsCdkWorkshopStack');
+new WorkshopPipelineStack(app, 'CdkWorkshopPipelineStack');
---
+++ lib/pipeline-stack.ts
@@ -0,0 +1,10 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+
+export class WorkshopPipelineStack extends cdk.Stack {
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        // Pipeline code goes here
+    }
+}
