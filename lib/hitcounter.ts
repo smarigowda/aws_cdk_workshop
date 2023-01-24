d50571e8cc2b42dbc14dd57aaa21830ca74d1a82
Santosh Marigowda <santosh.marigowda@abcam.com>
2023-01-24 15:45:23 +0000

    added TableViewer construct

--- lib/hitcounter.ts
+++ lib/hitcounter.ts
@@ -11,6 +11,9 @@ export class HitCounter extends Construct {
   /** allows accessing the counter function */
   public readonly handler: lambda.Function;
 
+  /** the hit counter table */
+  public readonly table: dynamodb.Table;
+
   constructor(scope: Construct, id: string, props: HitCounterProps) {
     super(scope, id);
 
@@ -18,6 +21,8 @@ export class HitCounter extends Construct {
       partitionKey: { name: "path", type: dynamodb.AttributeType.STRING },
     });
 
+    this.table = table;
+    
     this.handler = new lambda.Function(this, "HitCounterHandler", {
       runtime: lambda.Runtime.NODEJS_14_X,
       handler: "hitcounter.handler",
